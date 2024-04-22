ce3ab4a0f9623a168bbbb3e0bd6d54f7276acee0
ADILRADIDI <adilradidi212@gmail.com>
2024-04-22 16:33:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// event input search
+let search = document.getElementById("search_link");
+let input_Search = document.getElementById("input_search");
+search.addEventListener("click", () => {
+  if (input_Search.style.display === "none") {
+    input_Search.style.display = "block";
+  } else {
+    input_Search.style.display = "none";
+  }
+});
+
+// event broghtness and dark mode
+let btn_brightness = document.getElementById("brightness_icon");
+let img_dark = document.getElementById("img_dark");
+// select body
+let bd = document.getElementById("body");
+// select img search for change
+let img_search = document.getElementById("search_white");
+// select img search for change
+let img_location = document.getElementById("img_location");
+// event:-->
+btn_brightness.addEventListener("click", () => {
+  if (img_dark.src.includes("Sun.svg")) {
+    img_dark.src = "img/Moon.svg";
+    img_search.src = "img/search_white.svg";
+    img_location.src = "img/location_white.svg";
+    bd.style.color = "white";
+    bd.style.backgroundColor = "white";
+  } else {
+    img_dark.src = "img/Sun.svg";
+    img_search.src = "img/search_black.svg";
+    img_location.src = "img/location_black.svg";
+    bd.style.color = "black";
+    bd.style.backgroundColor = "black";
+  }
+});
