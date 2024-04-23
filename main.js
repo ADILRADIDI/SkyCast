1ef2031555dc6340b1bd9bd9a2e098eb93e9fed2
ADILRADIDI <adilradidi212@gmail.com>
2024-04-23 09:50:21 +0100

    second

--- main.js
+++ main.js
@@ -34,3 +34,79 @@ btn_brightness.addEventListener("click", () => {
     bd.style.backgroundColor = "black";
   }
 });
+
+// DATE
+// const city = document.getElementById("input_search");
+const apikey = "3d57b696db2bbf1337326cdf28fd0fd5";
+let days = [];
+const today = new Date();
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+for (let i = 0; i < 6; i++) {
+  today.setDate(today.getDate() + 1);
+  days.push(formatDate(today));
+}
+input_Search.addEventListener("change", function () {
+  console.log(days);
+});
+
+// api
+let latitude;
+let longitude;
+let city = input_Search.value;
+let metric = "units=metric";
+const api_key = "3d57b696db2bbf1337326cdf28fd0fd5";
+// const geoUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${api_key}`;
+// const cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+
+let fetchData = async function (url) {
+  try {
+    console.log(url);
+    const response = await fetch(`${url}`);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  if ("geolocation" in navigator) {
+    // Geolocation is supported
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        // Success callback
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        const geoUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${api_key}&${metric}`;
+        fetchData(`${geoUrl}`);
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      },
+      function (error) {
+        // Error callback
+        console.error("Error getting location: " + error.message);
+      },
+      {
+        // Options
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  } else {
+    // Geolocation is not supported
+    console.log("Geolocation is not supported by this browser.");
+  }
+});
+
+// const input_Search = document.getElementById("input-search");
+input_Search.addEventListener("change", () => {
+  const city = input_Search.value;
+  const cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&${metric}`;
+  fetchData(cityUrl);
+});
